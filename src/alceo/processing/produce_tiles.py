0ba9909ff2ca582c86dd05432e4ed85d258ca362
Gregory Sech <gregory.sech@iit.it>
2023-02-20 10:09:00 +0100

    use area of interest to produce vectorial tiles

--- src/alceo/processing/produce_tiles.py
+++ src/alceo/processing/produce_tiles.py
@@ -3,7 +3,7 @@ import rasterio
 from rasterio import windows
 from itertools import product
 import shapely.geometry
-from shapely import intersection_all
+from shapely import intersection_all, union_all, covered_by
 from pathlib import Path
 import geopandas as gpd
 from alceo.utils import in_notebook
@@ -14,6 +14,7 @@ def produce_tiles(
     input_paths,
     output_geojson_path,
     tile_prefix,
+    areas_of_interest_geojson: Path,
 ):
     #%% Loading all images bounds and find intersection
     all_bounds = []
@@ -26,6 +27,12 @@ def produce_tiles(
             _transform = dataset.transform
         if _crs is None:
             _crs = dataset.crs
+
+    assert (
+        areas_of_interest_geojson.exists()
+    ), f"Provided areas of intereset GeoJSON ({areas_of_interest_geojson}) does not exists!"
+    aoi_gdf = gpd.read_file(areas_of_interest_geojson, driver="GeoJSON").to_crs(_crs)
+    aoi_geom = union_all(aoi_gdf.geometry)
 
     all_geoms = [shapely.geometry.box(*bounds) for bounds in all_bounds]
     bounds_intersection = intersection_all(all_geoms)
@@ -55,14 +62,24 @@ def produce_tiles(
             height=tile_height,
         ).intersection(big_window)
         _window_bounds = windows.bounds(_window, _transform)
-        _tiles_geoms.append(shapely.geometry.box(*_window_bounds))
-        _tiles_data.append({"width": tile_width, "height": tile_height, "tile_id": f"{tile_prefix}({_window_bounds[0]}, {_window_bounds[1]})"})
+        tile_box = shapely.geometry.box(*_window_bounds)
+        if not covered_by(tile_box, aoi_geom):
+            continue
+        _tiles_geoms.append(tile_box)
+        _tiles_data.append(
+            {
+                "width": tile_width,
+                "height": tile_height,
+                "tile_id": f"{tile_prefix}({_window_bounds[0]}, {_window_bounds[1]})",
+            }
+        )
 
     # %% Create and save GeoDataFrame of tiles.
     tiles_gdf = gpd.GeoDataFrame(_tiles_data, geometry=_tiles_geoms, crs=_crs)
     tiles_gdf.to_file(output_geojson_path, index=False, driver="GeoJSON")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if not in_notebook():
         parser = ArgumentParser(
             "change_from_annotations.py",
@@ -93,16 +110,31 @@ if __name__ == '__main__':
             help="Prefix used for generating tile id. Defaults to 'tile_'.",
             default="tile_",
         )
+        parser.add_argument(
+            "-a",
+            "--areas_of_interest_geojson",
+            type=Path,
+            help="GeoJSON containing areas of interest that have to completely cover tiles to be rasterized.",
+            required=True,
+        )
 
         args = parser.parse_args()
         input_paths = args.input_paths
         output_geojson_path = args.output_geojson_path
         tile_prefix = args.tile_prefix
+        areas_of_interest_geojson = args.areas_of_interest_geojson
     else:
         input_paths = [
             "/home/gsech/Source/alceo/data/images/DURA_EUROPOS/DE_19_09_2014/DE_19_09_2014_NN_diffuse.tif",
             "/home/gsech/Source/alceo/data/images/DURA_EUROPOS/DE_26_5_2013/DE_26_5_2013_NN_diffuse.tif",
         ]
-        output_geojson_path = "/home/gsech/Source/alceo/data/images/DURA_EUROPOS/tiles.json"
+        output_geojson_path = (
+            "/home/gsech/Source/alceo/data/images/DURA_EUROPOS/tiles.json"
+        )
         tile_prefix = "tile_"
-    produce_tiles(input_paths, output_geojson_path, tile_prefix)
+    produce_tiles(
+        input_paths,
+        output_geojson_path,
+        tile_prefix,
+        areas_of_interest_geojson,
+    )
