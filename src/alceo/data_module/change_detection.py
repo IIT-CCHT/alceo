c608654e8ccbb870114b23a43d98f8458c46e6cd
Gregory Sech <gregory.sech@iit.it>
2023-04-20 14:44:54 +0200

    integrating dvc for training stage. geo-temporal data splits

--- src/alceo/data_module/change_detection.py
+++ src/alceo/data_module/change_detection.py
@@ -20,11 +20,13 @@ class AlceoChangeDetectionDataModule(pl.LightningDataModule):
         validation_labels: List[str] = [],
         test_paths: List[str] = [],
         test_labels: List[str] = [],
+        predict_paths: List[str] = [],
+        predict_labels: List[str] = [],
         batch_size: int = 16,
         num_workers: int = 5,
     ) -> None:
         """Data module for pits change detection. Labels will be used for creating tags for metric logging, except for training because they will be concatenated in a single training dataset.
-        
+
         Args:
             train_paths (List[str]): Paths to a list of training datasets in ALCEO change detection format. All these will be concatenated.
             train_labels (List[str]): Labels for the training datasets!
@@ -39,20 +41,30 @@ class AlceoChangeDetectionDataModule(pl.LightningDataModule):
         self.save_hyperparameters()
 
     def setup(self, stage: Optional[str] = None) -> None:
-        self.train_datasets, self.validation_datasets, self.test_datasets = [], [], []
+        (
+            self.train_datasets,
+            self.validation_datasets,
+            self.test_datasets,
+            self.predict_datasets,
+        ) = ([], [], [], [])
 
-        for  path in self.hparams.train_paths:
+        for path in self.hparams.train_paths:
             dataset = AlceoChangeDetectionDataset(Path(path))
             self.train_datasets.append(dataset)
-            
+
         for path in self.hparams.validation_paths:
             dataset = AlceoChangeDetectionDataset(Path(path))
             self.validation_datasets.append(dataset)
-            
+
         for path in self.hparams.test_paths:
             dataset = AlceoChangeDetectionDataset(Path(path))
             self.test_datasets.append(dataset)
             
+        for path in self.hparams.predict_paths:
+            dataset = AlceoChangeDetectionDataset(Path(path))
+            self.predict_datasets.append(dataset)
+                
+
         return super().setup(stage)
 
     def _dataloader_for_dataset(self, dataset: Dataset):
@@ -61,6 +73,13 @@ class AlceoChangeDetectionDataModule(pl.LightningDataModule):
             batch_size=self.hparams.batch_size,
             num_workers=self.hparams.num_workers,
         )
+        
+    def predict_dataloader(self) -> EVAL_DATALOADERS:
+        _dataloaders = [
+            self._dataloader_for_dataset(dataset)
+            for dataset in self.predict_datasets
+        ]
+        return _dataloaders
 
     def train_dataloader(self) -> TRAIN_DATALOADERS:
         return self._dataloader_for_dataset(ConcatDataset(self.train_datasets))
--- src/alceo/processing/pits_site_dataset.py
+++ src/alceo/processing/pits_site_dataset.py
@@ -1,16 +1,23 @@
 # %%
 import shutil
 import os
+from typing import List
 import geopandas as gpd
 import pandas as pd
 from argparse import ArgumentParser
 from pathlib import Path
 from glob import glob
+from shapely.ops import unary_union
 
 from alceo.utils import in_notebook
 
 # %%
-def pits_site_dataset(site_data_path: Path, output_path: Path):
+def pits_site_dataset(
+    site_data_path: Path,
+    output_path: Path,
+    area_selection_path: Path,
+    changes_selection: List[str],
+):
     # %%
     tiles_geojson_path = site_data_path / "tiles.geojson"
     tiles_folder_path = site_data_path / "tiles"
@@ -25,100 +32,139 @@ def pits_site_dataset(site_data_path: Path, output_path: Path):
         change_folder_path.exists()
     ), f"Provided change folder does not exists ({change_folder_path})"
 
-    assert output_path.exists(), f"Provided output path does not exists ({output_path})"
+    if area_selection_path is not None:
+        assert (
+            area_selection_path.exists()
+        ), f"Provided area selection GeoJSON does not exists ({area_selection_path})"
 
+    assert output_path.exists(), f"Provided output path does not exists ({output_path})"
+    # %%
+    change_dirs = []
+    if changes_selection is not None and len(changes_selection) > 0:
+        for change_sel in changes_selection:
+            change_sel_path = change_folder_path / change_sel
+            assert change_sel_path.exists(), f"Selected change folder doesn't exists {change_sel} ({change_sel_path})"
+            change_dirs.append(change_sel_path)
+    else:
+        for cs_dir in os.scandir(change_folder_path):
+            if not cs_dir.is_dir():
+                continue
+            change_start_dir = cs_dir.name
+            for ce_dir in os.scandir(change_folder_path / change_start_dir):
+                if not ce_dir.is_dir():
+                    continue
+                change_end_dir = ce_dir.name
+                change_dirs.append(Path(ce_dir))
+
+    # %%
+    tiles_gdf = gpd.read_file(tiles_geojson_path)
+    if area_selection_path is not None:
+        area_gdf = gpd.read_file(area_selection_path).to_crs(tiles_gdf.crs)
+        area_geom = unary_union(area_gdf.geometry)
+        tiles_gdf = tiles_gdf[tiles_gdf.covered_by(area_geom)]
+        
+    tiles_geom = unary_union(tiles_gdf.geometry)
     # %%
     vectorial_gdf = None
     _change_tiles_meta = []
-    for cs_dir in os.scandir(change_folder_path):
-        if not cs_dir.is_dir():
-            continue
-        change_start_dir = cs_dir.name
-        for ce_dir in os.scandir(change_folder_path / change_start_dir):
-            if not ce_dir.is_dir():
-                continue
-            change_end_dir = ce_dir.name
+    for change_dir in change_dirs:
+        change_end_dir = change_dir.name
+        change_start_dir = change_dir.parent.name
+        
+        vec_appeared = gpd.read_file(
+            change_folder_path
+            / change_start_dir
+            / change_end_dir
+            / "vectorial"
+            / "pits.appeared.geojson"
+        ).to_crs(tiles_gdf.crs)
+        vec_appeared["change_kind"] = "pits.appeared"
+        vec_appeared = vec_appeared[vec_appeared.within(tiles_geom)]
 
-            vec_appeared = gpd.read_file(
-                change_folder_path
-                / change_start_dir
-                / change_end_dir
-                / "vectorial"
-                / "pits.appeared.geojson"
+        vec_disappeared = gpd.read_file(
+            change_folder_path
+            / change_start_dir
+            / change_end_dir
+            / "vectorial"
+            / "pits.disappeared.geojson"
+        ).to_crs(tiles_gdf.crs)
+        vec_disappeared["change_kind"] = "pits.disappeared"
+        vec_disappeared = vec_disappeared[vec_disappeared.within(tiles_geom)]
+
+        vec_ = gpd.GeoDataFrame(
+            pd.concat([vec_appeared, vec_disappeared], ignore_index=True),
+        )
+        change = f"{change_start_dir}-{change_end_dir}"
+        vec_["change"] = change
+
+        if vectorial_gdf is None:
+            vectorial_gdf = vec_
+        else:
+            vectorial_gdf = gpd.GeoDataFrame(
+                pd.concat([vectorial_gdf, vec_], ignore_index=True)
             )
-            vec_appeared["change_kind"] = "pits.appeared"
 
-            vec_disappeared = gpd.read_file(
-                change_folder_path
-                / change_start_dir
-                / change_end_dir
-                / "vectorial"
-                / "pits.disappeared.geojson"
-            )
-            vec_disappeared["change_kind"] = "pits.disappeared"
-
-            vec_ = gpd.GeoDataFrame(
-                pd.concat([vec_appeared, vec_disappeared], ignore_index=True),
-            )
-            change = f"{change_start_dir}-{change_end_dir}"
-            vec_["change"] = change
-
-            if vectorial_gdf is None:
-                vectorial_gdf = vec_
-            else:
-                vectorial_gdf = gpd.GeoDataFrame(
-                    pd.concat([vectorial_gdf, vec_], ignore_index=True)
-                )
-
-            change_kind = "pits.appeared"
-            change_raster_dir = change_folder_path / change_start_dir / change_end_dir / "raster"
-            raster_glob_string = str(
+        change_kind = "pits.appeared"
+        change_raster_dir = (
+            change_folder_path / change_start_dir / change_end_dir / "raster"
+        )
+        raster_glob_string = str(
+            change_raster_dir / change_kind / "tiles" / "*.tif"
+        )
+        # %%
+        for appeared_tile_path in glob(raster_glob_string):
+            # Input paths!
+            appeared_tile_path = Path(appeared_tile_path)
+            if not (tiles_gdf.tile_id == appeared_tile_path.stem).any():
+                continue # The tile is not one of the selected tiles!
+            dis_tile_path = (
                 change_raster_dir
-                / change_kind
+                / "pits.disappeared"
                 / "tiles"
-                / "*.tif"
+                / appeared_tile_path.name
+            )
+            im1_in_path = (
+                tiles_folder_path / change_start_dir / appeared_tile_path.name
+            )
+            im2_in_path = (
+                tiles_folder_path / change_end_dir / appeared_tile_path.name
+            )
+            # %%
+            out_filename = "-".join(
+                [change_start_dir, change_end_dir, appeared_tile_path.name]
             )
 
-            for appeared_tile_path in glob(raster_glob_string):
-                # Input paths!
-                appeared_tile_path = Path(appeared_tile_path)
-                dis_tile_path = change_raster_dir / "pits.disappeared" / "tiles" / appeared_tile_path.name
-                im1_in_path = tiles_folder_path / change_start_dir / appeared_tile_path.name
-                im2_in_path = tiles_folder_path / change_end_dir / appeared_tile_path.name
-                # %%
-                out_filename = "-".join(
-                    [change_start_dir, change_end_dir, appeared_tile_path.name]
-                )
-                                
-                _change_tiles_meta.append(
-                    {
-                        "change": change,
-                        "change_start": change_start_dir,
-                        "change_end": change_end_dir,
-                        "tile_name": out_filename,
-                    }
-                )
+            _change_tiles_meta.append(
+                {
+                    "change": change,
+                    "change_start": change_start_dir,
+                    "change_end": change_end_dir,
+                    "tile_name": out_filename,
+                }
+            )
 
-                # Output paths
-                im1_out_path = output_path / "im1" / out_filename
-                im2_out_path = output_path / "im2" / out_filename
-                app_out_path = output_path / "pits.appeared" / out_filename
-                disapp_out_path = output_path / "pits.disappeared" / out_filename
+            # Output paths
+            im1_out_path = output_path / "im1" / out_filename
+            im2_out_path = output_path / "im2" / out_filename
+            app_out_path = output_path / "pits.appeared" / out_filename
+            disapp_out_path = output_path / "pits.disappeared" / out_filename
 
-                os.makedirs(im1_out_path.parent, exist_ok=True)
-                shutil.copy2(im1_in_path, im1_out_path)
+            os.makedirs(im1_out_path.parent, exist_ok=True)
+            shutil.copy2(im1_in_path, im1_out_path)
 
-                os.makedirs(im2_out_path.parent, exist_ok=True)
-                shutil.copy2(im2_in_path, im2_out_path)
+            os.makedirs(im2_out_path.parent, exist_ok=True)
+            shutil.copy2(im2_in_path, im2_out_path)
 
-                os.makedirs(app_out_path.parent, exist_ok=True)
-                shutil.copy2(appeared_tile_path, app_out_path)
-                
-                os.makedirs(disapp_out_path.parent, exist_ok=True)
-                shutil.copy2(dis_tile_path, disapp_out_path)
-    vectorial_gdf.to_file(output_path / "vectorial.geojson", index=False, driver="GeoJSON")
+            os.makedirs(app_out_path.parent, exist_ok=True)
+            shutil.copy2(appeared_tile_path, app_out_path)
+
+            os.makedirs(disapp_out_path.parent, exist_ok=True)
+            shutil.copy2(dis_tile_path, disapp_out_path)
+    vectorial_gdf.to_file(
+        output_path / "vectorial.geojson", index=False, driver="GeoJSON"
+    )
     pd.DataFrame(_change_tiles_meta).to_csv(output_path / "tiles_meta.csv", index=False)
-    
+
 
 if __name__ == "__main__":
     # %%
@@ -143,12 +189,36 @@ if __name__ == "__main__":
             help="Output folder path.",
             required=True,
         )
+        parser.add_argument(
+            "-a",
+            "--area_selection_path",
+            type=Path,
+            help="Path to geojson containing an area that contains all the selected tiles.",
+            default=None,
+        )
+        parser.add_argument(
+            "-c",
+            "--changes_selection",
+            nargs="+",
+            help="Change folders to select inside of the SITE/change/--- when building the dataset. Providing none means take all.",
+        )
 
         args = parser.parse_args()
-        site_path = args.site_path
+        site_data_path = args.site_path
         output_path = args.output_path
-    else:
-        site_path = Path("/HDD1/gsech/source/alceo/data/sites/DURA_EUROPOS")
-        output_path = Path("/HDD1/gsech/source/alceo/dataset/pits/DURA_EUROPOS")
+        area_selection_path = args.area_selection_path
+        changes_selection = args.changes_selection
 
-    pits_site_dataset(site_path, output_path)
+    else:
+        site_data_path = Path("/home/gsech/alceo/data/sites/ASWAN")
+        output_path = Path("/home/gsech/alceo/dataset/pits/test_ASWAN")
+        area_selection_path = Path(
+            "/home/gsech/alceo/data/sites/ASWAN/test_area.geojson"
+        )
+        changes_selection = ["AS_04_08_2005/AS_05_10_2015"]
+    # %%
+    pits_site_dataset(
+        site_data_path, output_path, area_selection_path, changes_selection
+    )
+
+# %%
